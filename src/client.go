341dc39fc53712d94e282fb283ae68994428fc03
roberto montalti <roberto.montalti00@gmail.com>
2021-09-20 14:49:37 +0200

    added simple do member function to handle binance api requests in a clean way

--- src/client.go
+++ src/client.go
@@ -10,11 +10,21 @@ type Client struct {
     httpClient *httpClient
 }
 
-func NewClient(apiKey string, apiSecret string) *Client {
+func NewClient(apiKey string, apiSecret string) (c *Client) {
     return &Client {
         apiKey: apiKey,
         apiSecrect: apiSecret,
         httpClient: &http.Client{}
     }
+}
+
+/*
+* TODO
+* Basically we want this "binance client" to call any given path and any given method, via this go
+* "member function"
+* inspired by: https://github.com/pdepip/go-binance/blob/master/binance/client.go
+*/
+func (c *Client) do(method string, fullUrl string, payload string, isAuth bool, result interface{}) (resp *http.Response) {
+    return nil
 }
 
