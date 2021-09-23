d990c59dee1d08100b3465e584ae7e3848193a36
roberto montalti <roberto.montalti00@gmail.com>
2021-09-23 17:47:13 +0200

    teest simple api call

--- src/main.go
+++ src/main.go
@@ -4,19 +4,31 @@ import (
     "fmt"
     "net/http"
     "io/ioutil"
+    "strconv"
 )
 
 var URL string = "https://httpbin.org/post"
+var TEST_API_KEY = "vmPUZE6mv9SD5VNHk4HlWFsOr6aKE2zvsw0MuIgwCIPy6utIco14y7Ju91duEh8A"
+var TEST_API_SECRET = "NhqPtmdSJYdKjVHjA7PZj4Mge3R5YNiP1e3UZjInClVN65XAbvqqM6A7H5fATj0j"
+
+type Res struct {
+    Mins int64 `json:"mins"`
+    Price string `json:"price"`
+}
 
 func main() {
-    binance := Binance{}
-    fmt.Printf(binance.systemStatus())
+    var res Res
+    client, err := NewClient(TEST_API_KEY, TEST_API_SECRET)
+    if err != nil {
+        panic(err)
+    }
+    client.do("GET", "/api/v3/avgPrice?symbol=BTCEUR", "", false, &res)
+    fmt.Printf("mins: " +  strconv.FormatInt(res.Mins, 10) + " | symbol: " + res.Price + "\n")
 }
 
 func PostTest() string {
     res, err  := http.Post(URL, "application/text", nil)
     data, err := ioutil.ReadAll(res.Body)
-
 
     if err != nil {
         fmt.Printf("error occurred")
