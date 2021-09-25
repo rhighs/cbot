02b30d96102f7f3154471de564579d02a5ded461
roberto montalti <roberto.montalti00@gmail.com>
2021-09-25 07:13:52 +0200

    cleaned funcs, removed io file read, since its already done in pathsloader.go

--- src/wallet.go
+++ src/wallet.go
@@ -2,41 +2,18 @@ package main
 
 import (
 	"cbot/responses" //gotta specify the mod name
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
 )
 
-func (c *Client) systemStatus() string {
-	var paths Paths
+func (c *Client) systemStatus() responses.AccountStatus {
 	var resp responses.AccountStatus
-	file, err := ioutil.ReadFile("../paths.json")
-	if err != nil {
-		panic(err)
-	}
-	err = json.Unmarshal([]byte(file), &paths)
-	res := c.do("GET", paths.Account.AccountStatus, true, &resp)
-	body, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		panic(err)
-	}
-	defer res.Body.Close()
-	return string(body)
+	c.do("GET", paths.Account.AccountStatus, true, &resp)
+	return resp
 }
 
 func (c *Client) accountInfo() responses.AccountInfo {
-	var paths Paths
 	var resp responses.AccountInfo
-	file, err := ioutil.ReadFile("../paths.json")
-
-	if err != nil {
-		panic(err)
-	}
-
 	fmt.Printf(paths.Account.AccountInfo)
-
-	err = json.Unmarshal([]byte(file), &paths)
 	c.do("GET", "/api/v3/account", true, &resp)
-
 	return resp
 }
