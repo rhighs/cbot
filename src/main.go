55de116a3d5fab26558620c11665e1111d5b0fb1
roberto montalti <roberto.montalti00@gmail.com>
2021-09-25 07:10:18 +0200

    removed test func

--- src/main.go
+++ src/main.go
@@ -4,21 +4,14 @@ import (
 	"fmt"
 	"io/ioutil"
     "encoding/json"
-	"net/http"
 )
 
-var URL string = "https://httpbin.org/post"
 var TEST_API_KEY = "8jFR3cMWfCWTLcBf01eyD523F38qUaTunv12dax8YJS6cOhVQerN5sJXgZOSNxQL"
 var TEST_API_SECRET = "sdYR2qyhhcNhMXy63K0ZohjwIQL7bCzkzsWT9PglcDaChHiDPzLY1hQRpfBgAPwT"
 
 type Keys struct {
     ApiKey string `json:"apiKey"`
     ApiSecret string `json:"apiSecret"`
-}
-
-type Res struct {
-	Mins  int64  `json:"mins"`
-	Price string `json:"price"`
 }
 
 func ParseKeysFile() Keys {
@@ -42,14 +35,4 @@ func main() {
 
 	resp := client.accountInfo()
 	fmt.Printf("%+v\n", resp)
-}
-
-func PostTest() string {
-	res, err := http.Post(URL, "application/text", nil)
-	data, err := ioutil.ReadAll(res.Body)
-
-	if err != nil {
-		fmt.Printf("error occurred")
-	}
-	return string(data)
 }
