bf38e60453833e4f4ed6ded680888600ad1da63a
Valerio Di Zio <valerio.dizio@studio.unibo.it>
2021-09-25 17:24:57 +0200

    accountInfo, work with key

--- src/main.go
+++ src/main.go
@@ -1,33 +1,33 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
-    "encoding/json"
 )
 
-var TEST_API_KEY = "8jFR3cMWfCWTLcBf01eyD523F38qUaTunv12dax8YJS6cOhVQerN5sJXgZOSNxQL"
-var TEST_API_SECRET = "sdYR2qyhhcNhMXy63K0ZohjwIQL7bCzkzsWT9PglcDaChHiDPzLY1hQRpfBgAPwT"
+var TEST_API_KEY = "vmPUZE6mv9SD5VNHk4HlWFsOr6aKE2zvsw0MuIgwCIPy6utIco14y7Ju91duEh8A"
+var TEST_API_SECRET = "NhqPtmdSJYdKjVHjA7PZj4Mge3R5YNiP1e3UZjInClVN65XAbvqqM6A7H5fATj0j"
 
 type Keys struct {
-    ApiKey string `json:"apiKey"`
-    ApiSecret string `json:"apiSecret"`
+	ApiKey    string `json:"apiKey"`
+	ApiSecret string `json:"apiSecret"`
 }
 
 func ParseKeysFile() Keys {
-    var keys Keys
-    file, err := ioutil.ReadFile("../keys.json")
-    if err != nil {
-        keys.ApiKey = TEST_API_KEY
-        keys.ApiSecret = TEST_API_SECRET
-    } else {
-        err = json.Unmarshal([]byte(file), &keys)
-    }
-    return keys
+	var keys Keys
+	file, err := ioutil.ReadFile("../keys.json")
+	if err != nil {
+		keys.ApiKey = TEST_API_KEY
+		keys.ApiSecret = TEST_API_SECRET
+	} else {
+		err = json.Unmarshal([]byte(file), &keys)
+	}
+	return keys
 }
 
 func main() {
-    keys := ParseKeysFile()
+	keys := ParseKeysFile()
 	client, err := NewClient(keys.ApiKey, keys.ApiSecret)
 	if err != nil {
 		panic(err)
